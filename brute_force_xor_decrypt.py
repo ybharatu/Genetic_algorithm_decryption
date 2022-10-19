09b8cf02284d62400d518bbbc71b9fbeeb5bca75
Yash Bharatula <ybharatu@purdue.edu>
2022-10-19 18:49:01 -0400

    Modified to tabulate results

--- brute_force_xor_decrypt.py
+++ brute_force_xor_decrypt.py
@@ -1,6 +1,8 @@
 import sys
 from BitVector import *
 from xor_cipher import *
+import glob
+from timeit import default_timer as timer
 
 # https://stackoverflow.com/questions/64890117/what-is-the-best-way-to-generate-all-binary-strings-of-the-given-length-in-pytho
 def gen_binary_strings(num_bits):
@@ -17,10 +19,10 @@ def recursive_gen_binary(num_bits, bin_str, base_str=""):
     recursive_gen_binary(num_bits, bin_str, base_str + "0")
     recursive_gen_binary(num_bits, bin_str, base_str + "1")
 
-def brute_force_xor_text(input_file, bits):
+def brute_force_xor_bin(input_file, bits, expected):
     with open(input_file, "r") as fptr:
         bin_encrypted = fptr.read()
-    expected = get_message()
+    #expected = get_message()
 
     bin_str = gen_binary_strings(bits)
     bin_encrypted = BitVector(bitstring=bin_encrypted)
@@ -29,12 +31,42 @@ def brute_force_xor_text(input_file, bits):
         temp_key = BitVector(bitstring=i)
         decrypted = xor_encrypt(temp_key, bin_encrypted)
         if decrypted == expected:
-            print("DECRYPTED! \nKey is: " + str(temp_key))
-            print(decrypted.get_text_from_bitvector())
-            break
+            #print("DECRYPTED! \nKey is: " + str(temp_key))
+            #print(decrypted.get_text_from_bitvector())
+            return temp_key
+    print("can't find key?")
+
+def brute_force_xor_test():
+    ################################################################
+    # Get all the encrypted outputs
+    ################################################################
+    all_results = []
+    for file in glob.glob("results/*.txt"):
+        print(file)
+        all_results.append(file)
+    all_results.sort(key=natural_keys)
+
+    expected = get_message()
+
+    j = 1
+    print("Number of bits   Max combinations checked   Total Time(sec)     Value of Key")
+    print("--------------   ------------------------   ---------------     ------------")
+    for i in all_results:
+        start = timer()
+        found_key = brute_force_xor_bin(i, j, expected)
+        end = timer()
+        print("{:<16d} {:<23d} {:>14.9f}         {:>s}".format(j, 2**j, end-start , str(found_key)))
+        j += 1
+
+
+    ################################################################
+    # Get the message
+    ################################################################
+    message = get_message()
 
 
 if __name__ == "__main__":
-    brute_force_xor_text(sys.argv[1], int(sys.argv[2]))
+    brute_force_xor_test()
+    #brute_force_xor_text(sys.argv[1], int(sys.argv[2]))
     #gen_binary_strings(3)
 
--- xor_cipher.py
+++ xor_cipher.py
@@ -2,6 +2,19 @@ import sys
 
 from BitVector import *
 import re
+import glob
+
+# https://stackoverflow.com/questions/5967500/how-to-correctly-sort-a-string-with-a-number-inside
+def atoi(text):
+    return int(text) if text.isdigit() else text
+
+def natural_keys(text):
+    '''
+    alist.sort(key=natural_keys) sorts in human order
+    http://nedbatchelder.com/blog/200712/human_sorting.html
+    (See Toothy's implementation in the comments)
+    '''
+    return [ atoi(c) for c in re.split(r'(\d+)', text) ]
 
 #################################################
 # Function Name: get_encryption_key
@@ -13,13 +26,13 @@ import re
 #       Reads file "key.txt" and extracts key and
 #       converts it into a text file
 ##################################################
-def get_encryption_key():
+def get_encryption_key(key_file_name="key.txt"):
 
-    with open("key.txt", "r") as fptr:
+    with open(key_file_name, "r") as fptr:
         key_str = fptr.read()
 
     #print("Key: " + key_str)
-    key = BitVector(bitstring=key_str)
+    key = BitVector(bitstring=key_str.strip())
     #key = BitVector(bitstring = key_str)
     #print(key.get_text_from_bitvector())
     print("Size of Key: " + str(len(key)) + "bit(s)")
@@ -58,7 +71,7 @@ def get_message():
 # Description:
 #       xor encrypts a message with the key
 ##################################################
-def xor_encrypt(key, message):
+def xor_encrypt(key, message, out_file_name="output_binary.txt", wren = False):
 
     key_length = len(key)
     mes_length = len(message)
@@ -73,8 +86,9 @@ def xor_encrypt(key, message):
         if key_pos >= key_length:
             key_pos = 0
 
-    with open("output_binary.txt", "w") as fptr:
-        fptr.write(str(out_encrypted))
+    if wren:
+        with open(out_file_name, "w") as fptr:
+            fptr.write(str(out_encrypted))
     # with open("output_text.txt", "w") as fptr:
     #     fptr.write(out_encrypted.get_text_from_bitvector())
     # with open("output_hex.txt", "w") as fptr:
@@ -82,11 +96,60 @@ def xor_encrypt(key, message):
 
     return out_encrypted
 
+#################################################
+# Function Name: gen_keys
+# Arguments:
+#   1. n_bits
+# Return:
+#   key_array
+# Description:
+#       Generates random keys for bits 1 to n
+##################################################
+def gen_keys(n_bits):
+    key_array = []
+    bv = BitVector(size=n_bits)
+    # For some reason, gen_random_bits doesn't work with a size < 3?
+    for i in range(3, n_bits, 1):
+        bv = bv.gen_random_bits(i)
+        print(str(bv))
+        key_array.append(bv)
+        with open("keys/key_" + str(i) +"_bits.txt", "w") as fptr:
+            fptr.write(str(bv))
+
+    with open("keys/all_keys.txt", "w") as fptr:
+        for i in key_array:
+            fptr.write(str(i) + "\n")
+
+
+def encrypt_xor_test():
+    ################################################################
+    # Get the message
+    ################################################################
+    message = get_message()
+
+    ################################################################
+    # Get all the keys
+    ################################################################
+    all_keys = []
+    for file in glob.glob("keys/key*.txt"):
+        all_keys.append(file)
+    all_keys.sort(key=natural_keys)
+    j = 1
+    for i in all_keys:
+        key = get_encryption_key(i)
+        print("Encrypting with " + str(key))
+        out = xor_encrypt(key, message, "results/output_" + str(j) + "_bits.txt", True)
+        j += 1
+
+
 if __name__ == "__main__":
 
-    key = get_encryption_key()
-    message = get_message()
-    out_encrypted = xor_encrypt(key, message)
+    gen_keys(15)
+    encrypt_xor_test()
+    # key = get_encryption_key()
+    # message = get_message()
+    # out_encrypted = xor_encrypt(key, message)
+
 
 
 
